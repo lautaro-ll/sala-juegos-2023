5a357d102b5b1e802a4c71a5aa9f93700cd0831e
Lautaro LL <lemos.lautaro@gmail.com>
2023-05-08 06:05:22 -0300

    Sprint 2 terminado

--- src/app/bienvenida/bienvenida.component.ts
+++ src/app/bienvenida/bienvenida.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AutenticacionService } from '../services/autenticacion.service';
+import { UsuariosService } from '../services/usuarios.service';
+import { Usuario } from '../models/usuario.model';
 
 @Component({
   selector: 'app-bienvenida',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienvenidaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private autenticacionService: AutenticacionService, private usuariosService: UsuariosService) { }
+
+  usuario?: Usuario;
+  id: string = "";
 
   ngOnInit(): void {
+    this.usuariosService.currentLoginState.subscribe(msg => this.id = msg);
+    this.traerUsuario(this.id);
   }
 
+  public async traerUsuario(id: string) {
+    await this.usuariosService.traer(id).then((respuesta) => {
+      this.usuario = respuesta;
+    });
+  }
+    
 }
